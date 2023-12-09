394a342f9b73e5298aa8f8e341e47de368e5e146
lggg123 <machinelearner2334@gmail.com>
2023-12-08 19:49:53 -0800

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import NftGallery from './components/NftGallery'; // Import NftGallery component
 
 function App() {
-  const heroImageUrl = `process.env.PUBLIC_URL + '/cover_image.jpg'`;  // Replace with your actual hero image URL
+  const heroImageUrl = process.env.PUBLIC_URL + '/cover_image.jpg';  // Replace with your actual hero image URL
   const openseaLink = "https://opensea.io/collection/your-collection"; // Replace with your OpenSea collection link
 
   return (
