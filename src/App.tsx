436e4f4d9eeae27a32c60009a3abb96e385d7480
lggg123 <machinelearner2334@gmail.com>
2023-12-08 18:58:33 -0800

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NftGallery from './components/NftGallery';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ghost NFT Giveaway</h1>
       </header>
+      <main>
+        <NftGallery />
+      </main>
     </div>
   );
 }
