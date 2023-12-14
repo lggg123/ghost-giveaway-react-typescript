57f1d9805837d7a0807cdf1887544087485ef9db
lggg123 <machinelearner2334@gmail.com>
2023-12-14 15:13:44 -0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@
 
 import React from 'react';
 import './App.css';
-import NftGallery from './components/NftGallery';
 import SignupForm from './components/signUpForm';
 import { useMediaQuery } from 'react-responsive';
 
 function App() {
   const heroImageUrl = process.env.PUBLIC_URL + '/cover_image.png';
   const heroImageUrlMobile = process.env.PUBLIC_URL + '/cover_image_mobile.png';
-  const openseaLink = 'https://opensea.io/collection/your-collection';
-  const title = 'GHOSTERZ';
+  // const openseaLink = 'https://opensea.io/collection/your-collection';
   const isMobile = useMediaQuery({ query: '(max-width: 768px'})
 
   return (
@@ -21,7 +19,7 @@ function App() {
         </div>
       </header>
       <main>
-      <p>Sign Up for your free Ghosterz NFT airdrop which drops December 24</p>
+      <p>Sign Up for your free Ghosterz NFT airdrop which drops December 24th!</p>
         <SignupForm />
       </main>
     </div>
